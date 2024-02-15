22e956ae84c73ebb11861f87f6846cb4ca64e05e
Jannick Ocampo <focampo3@gatech.edu>
2024-02-15 12:57:38 -0500

    pandas intro

---
+++ src/pandas/src/squirrel_count.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+squirrel_data = pd.read_csv('../resources/2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20240213.csv')
+
+gray_sq = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Gray"])
+cnnmn_sq = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Cinnamon"])
+blck_sq = len(squirrel_data[squirrel_data["Primary Fur Color"] == "black"])
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_sq, cnnmn_sq, blck_sq]
+}
+
+data = pd.DataFrame(data_dict)
+data.to_csv("../resources/short-list-squirrels.csv")
+
+
