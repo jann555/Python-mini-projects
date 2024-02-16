a39cfc5826f818ad764d24effd13a0a5d1c242d9
Jannick Ocampo <fredalocampo@gmail.com>
2024-02-16 15:56:06 -0500

    tkinker demo

---
+++ mini-projects/gui/tkinter/__init__.py
@@ -0,0 +1,42 @@
+"""
+ https://docs.python.org/3/library/tkinter.html#the-packer
+ https://tcl.tk/man/tcl8.6/TkCmd/pack.htm
+"""
+import tkinter
+
+
+def add(*args):
+    return sum(args)
+
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    return n
+
+
+kw_result = calculate(4, add=3, multiply=3)
+
+window = tkinter.Tk()
+window.title('First GUI Program')
+window.minsize(width=500, height=300)
+result = add(2, 5, 7, 8)
+my_label = tkinter.Label(text=f"*args Result {result}", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+def click():
+    msg = tk_input.get()
+    my_label.config(text=msg)
+
+
+button = tkinter.Button(text="click me", command=click)
+button.pack()
+
+# Entry
+
+tk_input = tkinter.Entry(width=10)
+tk_input.pack()
+
+window.mainloop()
