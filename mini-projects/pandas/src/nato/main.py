88e578f370539dccc7ec21463bcf1797c51e008a
Jannick Ocampo <focampo3@gatech.edu>
2024-02-18 17:10:58 -0500

    Handle exceptions

--- mini-projects/pandas/src/nato/main.py
+++ mini-projects/pandas/src/nato/main.py
@@ -4,8 +4,17 @@ data = pd.read_csv('../../resources/nato_phonetic_alphabet.csv')
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
 
-nato = [phonetic_dict[letter] for letter in word]
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
 
-print(nato)
+    try:
+        nato = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print('Only Errors in the Alphabet Please')
+        generate_phonetic()
+    else:
+        print(nato)
+
+
+generate_phonetic()
