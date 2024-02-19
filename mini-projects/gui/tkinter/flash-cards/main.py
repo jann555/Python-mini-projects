8a6f4761e79bfa6e9366092a92a037397f27f1e9
Jannick Ocampo <fredalocampo@gmail.com>
2024-02-19 16:37:38 -0500

    -Flashcard flipping

--- mini-projects/gui/tkinter/flash-cards/main.py
+++ mini-projects/gui/tkinter/flash-cards/main.py
@@ -1,11 +1,44 @@
+import random
 from tkinter import PhotoImage, Canvas, Button, Tk
 
+import pandas as pd
+
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+
+
+def initialize_cards():
+    # LOAD DATA
+    data_frame = pd.read_csv('./data/french_words.csv')
+    return data_frame.to_dict(orient="records")
+
+
+french_english_dict = initialize_cards()
+
+
+# Buttons commands
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(french_english_dict)
+    canvas.itemconfig(tittle_text, text=f'French', fill='black')
+    canvas.itemconfig(card_word, text=f'{current_card['French']}', fill='black')
+    canvas.itemconfig(flash_card, image=card_front)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(tittle_text, text=f'English', fill='white')
+    canvas.itemconfig(card_word, text=f'{current_card['English']}', fill='white')
+    canvas.itemconfig(flash_card, image=card_back)
+    canvas.update()
+
 
 window = Tk()
 window.title("Flash Card App")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR, highlightthickness=0)
-
+# Delay 3 seconds
+flip_timer = window.after(3000, func=flip_card)
 # Images from file
 card_front = PhotoImage(file='./images/card_front.png')
 card_back = PhotoImage(file='./images/card_back.png')
@@ -13,24 +46,17 @@ right = PhotoImage(file='./images/right.png')
 wrong = PhotoImage(file='./images/wrong.png')
 
 canvas = Canvas(width=800, height=526, background=BACKGROUND_COLOR, highlightbackground=BACKGROUND_COLOR)
-canvas.create_image(400, 263, image=card_front)
-canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
-canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+flash_card = canvas.create_image(400, 263, image=card_front)
+tittle_text = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
-
-# Buttons commands
-def right_command():
-    pass
-
-
-def wrong_command():
-    pass
-
-
 # Buttons
-wrong_button = Button(image=wrong, highlightthickness=0, command=wrong_command)
+wrong_button = Button(image=wrong, highlightthickness=0, command=next_card)
 wrong_button.grid(row=1, column=0)
-right_button = Button(image=right, highlightthickness=0, command=right_command)
+right_button = Button(image=right, highlightthickness=0, command=next_card)
 right_button.grid(row=1, column=1)
+
+next_card()
+
 window.mainloop()
