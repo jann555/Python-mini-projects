c84f1b6c3633e7a6317a2bca2f7f0ea60d5ac84e
Jannick Ocampo <fredalocampo@gmail.com>
2024-02-21 17:34:00 -0500

    Created Quizinterface class

--- mini-projects/apis/trivia/src/main.py
+++ mini-projects/apis/trivia/src/main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from quiz_brain import QuizBrain
 from utils.data import question_data
+from utils.ui import QuizInterface
 
 
 def execute():
@@ -12,9 +13,10 @@ def execute():
         question_bank.append(new_question)
 
     quiz = QuizBrain(question_bank)
+    quiz_ui = QuizInterface()
 
-    while quiz.still_has_questions():
-        quiz.next_question()
+    # while quiz.still_has_questions():
+    #     quiz.next_question()
 
     print("You've completed the quiz")
     print(f"Your final score was: {quiz.score}/{quiz.question_number}")
