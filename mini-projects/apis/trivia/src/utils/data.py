332f0a18cd64b66679955c01fa8e488f7b0da066
Jannick Ocampo <fredalocampo@gmail.com>
2024-02-22 16:28:50 -0500

    Completed Test

--- mini-projects/apis/trivia/src/utils/data.py
+++ mini-projects/apis/trivia/src/utils/data.py
@@ -5,7 +5,8 @@ response = requests.get(
     url='https://opentdb.com/api.php',
     params={
         'amount': 10,
-        'type': 'boolean'
+        'type': 'boolean',
+        'category': 18,
     })
 response.raise_for_status()
 
