dd710a8db43b4def9bb33b37947ae8bf77fb5414
Jannick Ocampo <focampo3@gatech.edu>
2024-03-02 17:49:41 -0500

    Created beautiful soup scrapping

--- mini-projects/web-scrapping/main.py
+++ mini-projects/web-scrapping/main.py
@@ -7,12 +7,20 @@ yc_webpage = response.text
 
 soup = BeautifulSoup(yc_webpage, parser='lxml', features="lxml")
 
-article_tag = soup.find(class_='titleline').find('a')
-article_text = article_tag.getText()
-artile_link = article_tag.get('href')
-article_upvote = soup.find(name='span', class_='score').getText()
+articles = soup.find_all('a')
+article_texts = []
+article_links = []
 
-print(article_tag)
-print(article_text)
-print(artile_link)
-print(article_upvote)
+print(articles)
+
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get('href')
+    article_links.append(link)
+
+article_up_votes = [int(score.getText().split(' ')[0]) for score in soup.find_all(name='span', class_='score')]
+
+print(article_texts)
+print(article_links)
+print(article_up_votes)
